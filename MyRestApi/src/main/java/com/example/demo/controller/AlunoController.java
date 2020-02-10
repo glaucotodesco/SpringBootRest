1e766b3ac5bb9c7b1a10ed6a2b5864cd8848b25b
Glauco Todesco <glauco.todesco@gmail.con>
2020-02-10 18:31:44 -0200

    Add Swagger

--- MyRestApi/src/main/java/com/example/demo/controller/AlunoController.java
+++ MyRestApi/src/main/java/com/example/demo/controller/AlunoController.java
@@ -2,16 +2,23 @@ package com.example.demo.controller;
 
 import java.util.List;
 import com.example.demo.model.*;
+
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.repository.AlunoRepository;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping(value = "/api")
@@ -45,7 +52,12 @@ public class AlunoController {
 		alunoRepository.delete(aluno);
 	}
 	
-	
+	@PutMapping("/aluno")
+	public Aluno upDateAluno(@RequestBody Aluno aluno)
+	{
+		return alunoRepository.save(aluno);
+		
+	}
 	
 	
 	
